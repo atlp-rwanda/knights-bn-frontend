a2f13b7c38b00e59e8ad911218ef6dbf35fdfadd
Kagorora <kagororamaxime@gmail.com>
2020-04-24 14:26:21 +0200

    bg(redux): fix redux
    -  add more redux setup functionality
    
    [Finishes #172497564]

--- src/route/App.js
+++ src/route/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Nav from '../components/Nav';
@@ -8,15 +9,15 @@ import LandingPage from '../containers/LandingPage';
 
 const App = () => (
   <div>
-      <Route>
-        <Nav />
-        <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/" exact component={Home} />
-          <Route path="/landing" exact component={LandingPage} />
-        </Switch>
-        <Footer />
-      </Route>
+    <Route>
+      <Nav />
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/" exact component={Home} />
+        <Route path="/landing" exact component={LandingPage} />
+      </Switch>
+      <Footer />
+    </Route>
   </div>
 );
 
