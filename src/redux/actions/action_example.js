5952c0927acd70f9cd459ef6f2317b61993ffd07
Kagorora <kagororamaxime@gmail.com>
2020-04-29 10:33:08 +0200

    ft(login): add Login feature
    - add Login functionality
    
    [Finishes #170947605]

--- src/redux/actions/action_example.js
+++
@@ -1,6 +0,0 @@
-const reduxTest = () => ({
-  type: 'FIRST_ACTION',
-  payload: 'test redux',
-});
-
-export default reduxTest;
--- src/route/App.js
+++ src/route/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import Home from '../containers/Home';
-import Login from '../containers/Login';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import LandingPage from '../containers/LandingPage';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import Home from "../containers/Home";
+import Login from "../views/LoginPage";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import LandingPage from "../containers/LandingPage";
+import HomePage from "../views/Home";
 
 const App = () => (
   <div>
@@ -15,6 +16,7 @@ const App = () => (
         <Route path="/login" exact component={Login} />
         <Route path="/" exact component={Home} />
         <Route path="/landing" exact component={LandingPage} />
+        <Route path="/Home" exact component={HomePage} />
       </Switch>
       <Footer />
     </Route>
